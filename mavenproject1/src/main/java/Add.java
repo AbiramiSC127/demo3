bf09789afbc09940f04ff24762dd132d90580954
Abirami <abirami.502202@sxcce.edu.in>
2025-05-10 11:25:19 +0530

    Add files via upload

---
+++ mavenproject1/src/main/java/Add.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author IT20
+ */
+
+public class Add{
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
